62db2122aad4134308cc78a6a1f31b1e18df7786
ValianxD <44631110+ValianxD@users.noreply.github.com>
2024-11-12 00:49:24 +0800

    DB connection

---
+++ db_connection.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('DB_SERVER')) {
+    define('DB_SERVER', 'localhost');
+}
+
+if (!defined('DB_USERNAME')) {
+    define('DB_USERNAME', 'root');
+}
+
+if (!defined('DB_PASSWORD')) {
+    define('DB_PASSWORD', '');
+}
+
+if (!defined('DB_DATABASE')) {
+    define('DB_DATABASE', 'smallbrain');
+}
+// Create connection
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
